11b408e8c006a6e33e8f486d9c653f928bdd2959
dbstj0403 <wcn09088@naver.com>
2025-08-04 15:44:28 +0900

    chore: modify api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,39 +1,39 @@
-import axios from 'axios';
-import { Product } from '../types/Product';
+import axios from "axios";
+import { Product } from "../types/Product";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: "http://54.206.153.118:8080/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 // 요청 인터셉터: 토큰 자동 삽입
 api.interceptors.request.use(
-    config => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    error => Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 // 실제 API 호출 정의
 export const productService = {
-    getAll: (): Promise<Product[]> =>
-        api.get('/products').then(res => res.data),
+  getAll: (): Promise<Product[]> =>
+    api.get("/products").then((res) => res.data),
 
-    getById: (id: number): Promise<Product> =>
-        api.get(`/products/${id}`).then(res => res.data),
+  getById: (id: number): Promise<Product> =>
+    api.get(`/products/${id}`).then((res) => res.data),
 
-    create: (data: Omit<Product, 'id'>): Promise<Product> =>
-        api.post('/products', data).then(res => res.data),
+  create: (data: Omit<Product, "id">): Promise<Product> =>
+    api.post("/products", data).then((res) => res.data),
 
-    update: (id: number, data: Partial<Omit<Product, 'id'>>): Promise<Product> =>
-        api.put(`/products/${id}`, data).then(res => res.data),
+  update: (id: number, data: Partial<Omit<Product, "id">>): Promise<Product> =>
+    api.put(`/products/${id}`, data).then((res) => res.data),
 
-    delete: (id: number): Promise<void> =>
-        api.delete(`/products/${id}`).then(() => {}),
+  delete: (id: number): Promise<void> =>
+    api.delete(`/products/${id}`).then(() => {}),
 };
